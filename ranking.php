261cf6be3460af3036890045d91f6bbafc8f1c46
mitten-dapper <mittendapper@gmail.com>
2021-08-19 23:32:41 -0400

    stuff

--- ranking.php
+++ ranking.php
@@ -16,19 +16,17 @@
     $con=mysqli_connect($hostname, $username, $password, $database);
 
 	$i=1;
-	$query="Select * from photos  order by rating desc limit 50";
+	$query="Select * from photos  order by rating desc limit 100";
 	$sql=mysqli_query($con, $query);
     if ($con->connect_error) {
         echo "Error";
     }
 	while($row=mysqli_fetch_array($sql, MYSQLI_ASSOC))
 	{	
-		echo "<center>";
-		echo " -----------------".$i." -----------------";
 		echo '<div id="photoRandom-small"><img class="image" src="images/'.$row['photo'].'"></br>';
+		echo "RANK : <b>".$i."</b><br>";
 		echo "Current rating : <b>".$row['rating']."</b><br>";
-        echo "Details: <a href='https://www.larvalabs.com/cryptopunks/details/".$row['id']."'>https://www.larvalabs.com/cryptopunks/details/".$row['id']."</a></br>";
-		echo "</center>";
+        echo "<a href='https://www.larvalabs.com/cryptopunks/details/".$row['id']."'>https://www.larvalabs.com/cryptopunks/details/".$row['id']."</a></br>";
 		$i++;
 	}
 
